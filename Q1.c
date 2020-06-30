e7f1d28120cd7f30bbbcad50fc5c26f2c14f5e0e
Linshengyi-William <67583611+Linshengyi-William@users.noreply.github.com>
2020-06-30 16:23:07 -0700

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,115 @@
+/* CS261- Assignment 1A - Question 1
+   Name: 
+   Date: 6/28/2020
+   Solution description:
+*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+struct student
+{
+	int id;
+	int score;
+};
+
+struct student* allocate()
+{
+     struct student *std_arr = malloc(10 * sizeof(struct student));
+     //assert(std_arr != 0);
+
+     return std_arr;
+     // Allocate memory for ten students*/
+     
+     // Return the pointer*/
+}
+
+void generate(struct student* students)
+{
+     int i;
+     srand(time(NULL));
+     for(i = 0; i < 10; i++)
+     {
+          students[i].id = rand()%10 + 1;
+          students[i].score = rand()%100 + 1;
+          //if(students->id == NULL )
+     }
+     /* Generate random and unique IDs and random scores for ten students, 
+		IDs being between 1 and 10, scores between 0 and 100 */
+     
+}
+
+void output(struct student* students)
+{
+     int i;
+
+     printf("*** Students Information ***\n");
+     for(i = 0; i < 10; i++)
+     {
+          printf("* Student %d", i);
+          printf("   ID: %d", students[i].id);
+          printf("   Score: %d\n", students[i].score);
+
+
+     }
+     /* Output information about the ten students in the format:
+        ID1 Score1
+        ID2 score2
+        ID3 score3
+        ...
+        ID10 score10*/
+}
+
+void summary(struct student* students)
+{
+     int i;
+     int min = 0, max = 0, sum = 0, average = 0;
+     for(i = 0; i < 10; i++)
+     {
+          if(i == 0)
+               min = students[0].score;
+          if(students[i].score > max)
+               max = students[i].score;
+          if(min > students[i].score)
+               min = students[i].score;
+          sum = sum + students[i].score;
+     }
+     average = sum / 10;
+
+     //printf("\n");
+     printf("\n** Minimum score: %d\n", min);
+     printf("** Maximum score: %d\n", max);
+     printf("** Average Score: %d\n", average);
+
+     
+     /* Compute and print the minimum, maximum and average scores of the 
+		ten students */
+     
+}
+
+void deallocate(struct student* stud)
+{
+     free(stud);
+     // Deallocate memory from stud
+}
+
+int main()
+{
+    struct student* stud = NULL;
+    
+    // Call allocate
+    stud = allocate();
+    // Call generate
+    generate(stud);
+    // Call output
+    output(stud);
+    // Call summary
+    summary(stud);
+    // Call deallocate
+    deallocate(stud);
+
+    return 0;
+}
+
---
+++ Q3.c
@@ -0,0 +1,210 @@
+/* CS261- Assignment 1A - Question 3
+   Name: Linshengyi Sun Partner: Samantha Rodarte
+   Date: 6/29/2020
+   Solution description: Convert the string into the camelcase format, print it out.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char toUpperCase(char ch)
+{
+  // Convert ch to upper case, assuming it is in lower case currently
+  if(ch >= 'a' && ch <= 'z')
+    ch = ch - 32;
+  return ch;
+}
+
+char toLowerCase(char ch)
+{
+  if(ch >= 'A' && ch <='Z')
+    ch = ch + 32;
+  // Convert ch to lower case, assuming it is in upper case currently                          
+  return ch;
+}
+
+int stringLength(char s[])
+{
+
+  int length = 0;
+  while (s[length] != '\0')
+  {
+    length++;
+  }
+
+  return length;
+   // Return the length of the string
+}
+
+int is_all_letter(char* word)
+{
+	int length = stringLength(word);
+  //int true_or_false = 0;
+  int letter_num = 0;
+  int i = 0;
+  while(i != length)
+  {
+		if((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+    {
+      letter_num = letter_num + 1;
+		}
+    ++i;
+	}
+  if((letter_num + 1) == length) // + 1 is to include '\n'
+    return 0;
+  else
+    return 1;
+  
+}
+
+int is_all_symbol(char* word)
+{
+  int length = stringLength(word);
+  int symbol_num = 0;
+  int i = 0;
+  while(i != length)
+  {
+		if(!(word[i] >= 65 && word[i] <= 90) && !(word[i] >= 97 && word[i] <= 122)) 
+    {
+      symbol_num = symbol_num + 1;
+		}
+    ++i;
+	}
+  
+  if(symbol_num == length) // No "+1" since '\0' is non-letter.
+    return 0;
+  else
+    return 1;
+}
+
+int is_symbol_in_between(char* word)
+{
+  int length = stringLength(word);
+  int true_or_false = 0;
+  for(int i = 1; i < length - 2; i++)
+  {
+    if(!(word[i] >= 65 && word[i] <= 90) && !(word[i] >= 97 && word[i] <= 122))
+    {
+      true_or_false = 1;
+      break;
+    }
+  }
+  return true_or_false;
+}
+void convert_to_underscore_and_lowercase(char* word) 
+{
+	int length = stringLength(word);
+	//int j,k,t;
+  int i = 0;
+  while(i != length)
+  {
+		if (!(word[i] >= 65 && word[i] <= 90) && !(word[i] >= 97 && word[i] <= 122)) 
+    {
+			word[i] = '_';
+		}
+		else
+    {
+      word[i] = toLowerCase(word[i]);
+    }
+    ++i;
+	}
+}
+void convert_to_letter_group_underscore(char* word)
+{
+  int length = stringLength(word);
+  //int new_word_length = length;
+  char new_word[100];
+  int i = 0;
+  int new_i = 0;
+  while(i != length)
+  {
+    if((word[i] >= 65 && word[i] <= 90) || (word[i] >= 97 && word[i] <= 122))
+    {
+      new_word[new_i] = word[i];
+      new_i++;
+      if(word[i+1] == '_' && new_i != length - 1)
+      {
+        new_word[new_i] = '_';
+        new_i++;
+      }
+      
+    }
+    ++i;
+  }
+  //int new_word_length = stringLength(new_word);
+  //new_word[new_word_length-1] = '\0';
+  /*
+  if(new_word[new_i] == '_' &&new_word[new_i+1] == '\0')
+  {
+    new_word[new_i] = '\0';
+  }
+  
+  //printf("New word length: %d \n", new_word_length);
+  */
+  printf("After first converting: %s \n", new_word);
+  
+
+
+}
+void camelCase(char* word)
+{
+	// Convert to camelCase
+	
+	//Check if the string is valid
+	if(is_all_letter(word) == 0 || is_all_symbol(word) == 0 || is_symbol_in_between(word) == 0)
+  {
+		printf("Invalid string. \n");
+	}
+  else
+	/*Convert all non letter character to underscores;
+    convert all letter character to lowercase. */
+	{
+    convert_to_underscore_and_lowercase(word);
+
+    //convert the string to the format of lettergroup + underscore + lettergroup
+    convert_to_letter_group_underscore(word);
+
+	  //Start to convert it to the final version
+	  int t, length = stringLength(word);
+    int i = 0;
+	  for(int i = t = 0; i<length;++i)
+	  {
+      if(word[i] == '_')
+		  {
+        word[i+1] = toUpperCase(word[i+1]);
+			  word[i] = '\0';
+		  }
+      else
+		  {
+			  word[t++] = word[i];
+		  }
+    }
+	  //Adjust the word
+	  word[0] = toLowerCase(word[0]);
+    int j = length - 1;
+    while(j >= t)
+    {
+      word[j] = '\0';
+      j--;
+    }
+  }
+}
+
+int main()
+{
+
+	// Read the string from the keyboard
+  char s[100];
+	printf("Please enter a string.\n");
+  fgets(s,100,stdin);
+
+	// Call camelCase
+	camelCase(s);
+  
+	
+	// Print the new string
+  printf("After converting: %s \n ", s);
+	
+	
+	return 0;
+}
